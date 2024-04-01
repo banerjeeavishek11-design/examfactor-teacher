e2e4abe4c1d6ffaff18349c0ff46b64ff69f87ea
Saptarshi Singha Roy <Saptarshi.singharoy07@gmail.com>
2024-04-01 13:24:22 +0530

    design bug fix 1/04

--- src/components/template/Concentrix/Concentrix.jsx
+++ src/components/template/Concentrix/Concentrix.jsx
@@ -123,7 +123,8 @@ const Concentrix = (props) => {
                 },
               ]}
             >
-              {scorePercentage}/100
+              {scorePercentage}
+              <Text style={[fonts.size_12,fonts.fontWeight_small,{color: colors.gray200}]}>/100</Text>
             </Text>
           ) : (
             <Text
