41570a18e731cc6b1c7a3162cef78df6fb9a90b5
Avishek Banerjee <avishek.banerjee@highkeen.com>
2024-05-31 13:24:54 +0530

    bug fix

--- src/screens/Home/SubjectDetailsScreen.jsx
+++ src/screens/Home/SubjectDetailsScreen.jsx
@@ -127,9 +127,9 @@ const SubjectDetailsScreen = () => {
           </View>
         </LinearGradient>
         {!searchChapterName && (
-          <View style={[layout.itemsCenter, layout.justifyCenter, { height: 600 }]}>
+          <View style={[layout.itemsCenter, layout.justifyCenter, { height: 500 }]}>
             <Text style={[fonts.fontWeignt_600, fonts.size_20, { color: colors.white }]}>
-              No Data
+              No Chapters Found
             </Text>
           </View>
         )}
